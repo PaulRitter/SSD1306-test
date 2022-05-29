944b166303272ddcb4003e2ff5ee4d0294fcdd14
Paul <ritter.paul1@googlemail.com>
2022-05-29 20:36:42 +0200

    a

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ import adafruit_ssd1306
 import digitalio
 from PIL import Image, ImageDraw, ImageFont
 
+BORDER = 5
+
 oled_reset = digitalio.DigitalInOut(board.D4)
 i2c = board.I2C()
 oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, #addr=0x3d, 
